ea329a9f3126fa5161c88745e83f9f8a3814c0f4
YYQ <531873600@qq.com>
2018-06-26 17:50:47 +0800

    第九章  聊天界面的外部边框搭建完成

---
+++ app/src/main/java/net/qiujuer/italker/push/frags/message/ChatGroupFragment.java
@@ -0,0 +1,25 @@
+package net.qiujuer.italker.push.frags.message;
+
+
+import android.support.v4.app.Fragment;
+
+import net.qiujuer.italker.push.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatGroupFragment extends ChatFragment {
+
+
+    public ChatGroupFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.fragment_chat_group;
+    }
+
+}
